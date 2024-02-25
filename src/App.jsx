8eda3741c7c6c2c0b8858e5d09478b70f4cf4c3d
AlexisIsla <alexis.u.isla@gmail.com>
2024-02-25 00:48:29 -0300

    first render threejs

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { Experience } from './components/Experience'
+
+function App() {
+  
+
+  return (
+    <Canvas>
+      <Experience/>
+    </Canvas>
+  )
+}
+
+export default App
---
+++ src/components/Experience.jsx
@@ -0,0 +1,13 @@
+import { OrbitControls } from "@react-three/drei"
+import { Office } from "./Office";
+
+export const Experience = () => {
+    return (
+        <>
+        <ambientLight intensity={1}/>
+        <OrbitControls/>
+        <Office/>
+        </>
+    );
+};
+
